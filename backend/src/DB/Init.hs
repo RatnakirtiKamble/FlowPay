15e8a7aa6015d771f35b2c36f6fd760dbbdb7213
ratnakirti <ratnakirti1234@gmail.com>
2025-08-10 20:02:41 +0530

    Added docker file

--- backend/src/DB/Init.hs
+++ backend/src/DB/Init.hs
@@ -4,12 +4,18 @@
 module DB.Init (getDBConnection) where
 
 import Database.PostgreSQL.Simple (Connection, connect, defaultConnectInfo, connectHost, connectUser, connectPassword, connectDatabase)
-
+import System.Environment (lookupEnv)
+import Data.Maybe (fromMaybe)
 -- | Creates and returns a new connection to the PostgreSQL database.
 getDBConnection :: IO Connection
-getDBConnection = connect defaultConnectInfo
-    { connectHost     = "localhost"
-    , connectUser     = "ratna"
-    , connectPassword = "Ratna@1234"
-    , connectDatabase = "payments_db"
-    }
+getDBConnection = do 
+    host <- fromMaybe "localhost" <$> lookupEnv "PGHOST"
+    db   <- fromMaybe "payments_db" <$> lookupEnv "PGDATABASE"
+    user <- fromMaybe "postgres" <$> lookupEnv "PGUSER"
+    pass <- fromMaybe "yourpassword" <$> lookupEnv "PGPASSWORD"
+    connect defaultConnectInfo
+        { connectHost = host
+        , connectDatabase = db
+        , connectUser = user
+        , connectPassword = pass
+        }
